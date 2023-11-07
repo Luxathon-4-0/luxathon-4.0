8da592a3ec845dc335c2588674a501f9cab7f775
IEEESBCEK2000 <60155072+IEEESBCEK2000@users.noreply.github.com>
2023-11-07 19:49:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+
+TweenMax.staggerFrom("nav ul li", 1, {
+  opacity: 0,
+  delay: 2.2,
+  x: -20,
+  ease: Power3.easeInOut
+}, 0.8)
+
+TweenMax.from(".logo", 1, {
+  opacity: 0,
+  delay: 2,
+  x: -20,
+  ease: Expo.easeInOut
+})
+TweenMax.from(".title", 1, {
+  opacity: 0,
+  delay: 2.75,
+  y: 20,
+  ease: Expo.fadeIn
+})
+TweenMax.from(".img", 2, {
+  opacity: 0,
+  delay: 1,
+  ease: Expo.fadeIn   
+})
+
 var countDownDate = new Date("Dec 8 ,2023    09:00:00").getTime();
 var x = setInterval(function() {
   var now = new Date().getTime();
@@ -17,3 +43,21 @@ document.getElementById("Sec").innerHTML=seconds;
     document.getElementById("counter").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+
+
+const observer= new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+    console.log(entry)
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+    else{
+      entry.target.classList.add('hide');
+
+    }
+  });
+});
+
+const hiddenElements= document.getElementById('img');
+hiddenElements.forEach((el)=>observer.observe(el))
